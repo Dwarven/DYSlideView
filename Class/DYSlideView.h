54040f3ad1dbc72b3d34140de74d17d19f0fc85e
Dwarven YANG <prison.yang@gmail.com>
2016-04-18 20:19:16 +0800

    add DYSlideView

--- Class/DYSlideView.h
+++ Class/DYSlideView.h
@@ -8,6 +8,36 @@
 
 #import <UIKit/UIKit.h>
 
+
+@protocol DYSlideViewDelegate <NSObject>
+
+@required
+- (NSInteger)DY_numberOfViewControllersInSlideView;
+- (nonnull NSArray *)DY_titlesForViewControllersInSlideView;
+- (nonnull NSArray *)DY_viewControllersInSlideView;
+
+@optional
+- (void)DY_didSelectButtonAtIndex:(NSInteger)index;
+
+@end
+
+
 @interface DYSlideView : UIView
+
+@property (nonatomic, strong, nonnull) id <DYSlideViewDelegate> delegate;
+
+@property (strong, nonatomic, nonnull) UIColor *slideBarColor;
+@property (nonatomic) CGFloat slideBarHeight;
+
+@property (strong, nonatomic, nonnull) UIColor *sliderColor;
+@property (nonatomic) CGFloat sliderHeight;
+@property (nonatomic) CGFloat sliderScale;
+
+@property (strong, nonatomic, nonnull) UIColor *buttonNormalColor;
+@property (strong, nonatomic, nonnull) UIColor *buttonSelectedColor;
+@property (strong, nonatomic, nonnull) UIFont *buttonTitleFont;
+
+@property (nonatomic) BOOL scrollViewBounces;
+
 
 @end
--- Class/DYSlideView.m
+++ Class/DYSlideView.m
@@ -8,14 +8,157 @@
 
 #import "DYSlideView.h"
 
+@interface DYSlideView () <UIScrollViewDelegate> {
+    NSInteger _numberOfViewControllers;
+    NSArray * _titlesForViewControllers;
+    BOOL _didSelectItemAtIndexDefined;
+    UIView * _slideBar;
+    UIView * _slider;
+    NSMutableArray *_buttonsArray;
+    UIButton * _selectedButton;
+    NSInteger _currentBtnIndex;
+    UIScrollView *_scrollView;
+}
+
+@end
+
 @implementation DYSlideView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)setDelegate:(id<DYSlideViewDelegate>)delegate {
+    _delegate = delegate;
+    _didSelectItemAtIndexDefined = [_delegate respondsToSelector:@selector(DY_didSelectButtonAtIndex:)];
 }
-*/
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        _slideBarColor = [UIColor lightGrayColor];
+        _sliderColor = [UIColor redColor];;
+        _buttonNormalColor = [UIColor whiteColor];
+        _buttonSelectedColor = [UIColor blackColor];
+        _sliderScale = 1.f;
+        _slideBarHeight = 50.f;
+        _sliderHeight = 4.f;
+        _buttonTitleFont = [UIFont systemFontOfSize:16.f];
+        _scrollViewBounces = YES;
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    _numberOfViewControllers = [self.delegate DY_numberOfViewControllersInSlideView];
+    _titlesForViewControllers = [self.delegate DY_titlesForViewControllersInSlideView];
+    [self addSlideBar];
+    [self addScrollView];
+    [self updateSelectedButton];
+    [_selectedButton setTitleColor:_buttonSelectedColor forState:UIControlStateNormal];
+}
+
+- (void)addSlideBar {
+    if (!_slideBar) {
+        _slideBar = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, _slideBarHeight)];
+        [_slideBar setBackgroundColor:_slideBarColor];
+        [self addSubview:_slideBar];
+    }
+    [self addButtons];
+    [self addSlider];
+}
+
+- (void)addButtons {
+    _buttonsArray = [NSMutableArray array];
+    for (NSInteger i = 0; i < _numberOfViewControllers; i++) {
+        
+        CGFloat width = self.bounds.size.width / _numberOfViewControllers;
+        
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        [button setFrame:CGRectMake( width * i, 5, width, 35)];
+        [button setTag:i];
+        [button.titleLabel setTextAlignment:NSTextAlignmentCenter];
+        [button.titleLabel setFont:_buttonTitleFont];
+        [button setTitleColor:_buttonNormalColor forState:UIControlStateNormal];
+        [button setTitle:[_titlesForViewControllers objectAtIndex:i] forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [_buttonsArray addObject:button];
+        [_slideBar addSubview:button];
+    }
+}
+
+- (void)addSlider {
+    if (!_slider) {
+        CGFloat buttonWidth = self.bounds.size.width / _numberOfViewControllers;
+        CGFloat sliderWidth = buttonWidth * _sliderScale;
+        
+        _slider = [[UIView alloc] initWithFrame:CGRectMake((buttonWidth - sliderWidth)/2, _slideBarHeight-_sliderHeight, sliderWidth, _sliderHeight)];
+        [_slider setBackgroundColor:_sliderColor];
+        [_slideBar addSubview:_slider];
+    }
+}
+
+- (void)addScrollView {
+    _scrollView = [[UIScrollView alloc] init];
+    [_scrollView setFrame:CGRectMake(0, _slideBarHeight, self.bounds.size.width, self.frame.size.height - _slideBarHeight)];
+    [_scrollView setDirectionalLockEnabled:YES];
+    [_scrollView setPagingEnabled:YES];
+    [_scrollView setContentSize:CGSizeMake(_scrollView.frame.size.width * _numberOfViewControllers, 0)];
+    [_scrollView setShowsHorizontalScrollIndicator:NO];
+    [_scrollView setDelegate:self];
+    [_scrollView setBounces:_scrollViewBounces];
+    
+    [self addSubview:_scrollView];
+    
+    NSArray *cvcs = [self.delegate DY_viewControllersInSlideView];
+    for (UIViewController *vc in cvcs) {
+        CGRect rect = _scrollView.bounds;
+        rect.origin.x = self.bounds.size.width * [cvcs indexOfObject:vc];
+        [vc.view setFrame:rect];
+        [_scrollView addSubview:vc.view];
+    }
+}
+
+- (void)buttonClicked:(UIButton *)button {
+    [_scrollView setContentOffset:CGPointMake(self.bounds.size.width * button.tag, 0) animated:YES];
+}
+
+- (void)updateSelectedButton{
+    if (!(_selectedButton && [_buttonsArray indexOfObject:_selectedButton] == _currentBtnIndex)) {
+        _selectedButton = [_buttonsArray objectAtIndex:_currentBtnIndex];
+        if (_didSelectItemAtIndexDefined) {
+            [self.delegate DY_didSelectButtonAtIndex:[_selectedButton tag]];
+        }
+    }
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGPoint contentOffset = scrollView.contentOffset;
+    
+    CGFloat buttonWidth = self.bounds.size.width / _numberOfViewControllers;
+    CGFloat sliderWidth = buttonWidth * _sliderScale;
+    CGRect rect = _slider.frame;
+    rect.origin.x = (contentOffset.x/self.bounds.size.width) * buttonWidth + (buttonWidth - sliderWidth)/2;
+    [_slider setFrame:rect];
+    
+    CGFloat ratio = contentOffset.x/self.bounds.size.width;
+    NSInteger index = (NSInteger)ratio;
+    if (ratio - (NSInteger)ratio >= 0.5) {
+        index += 1;
+    }
+    
+    if (_currentBtnIndex != index) {
+        _currentBtnIndex = index;
+        for (UIButton *button in _buttonsArray) {
+            if ([_buttonsArray indexOfObject:button] == _currentBtnIndex) {
+                [[_buttonsArray objectAtIndex:_currentBtnIndex] setTitleColor:_buttonSelectedColor forState:UIControlStateNormal];
+            } else {
+                [button setTitleColor:_buttonNormalColor forState:UIControlStateNormal];
+            }
+        }
+    }
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    [self updateSelectedButton];
+}
 
 @end
